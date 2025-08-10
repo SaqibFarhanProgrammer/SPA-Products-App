84ac9caf4e31428d384f9dafcafd7f80c5312651
shahzaib khan <shahzaibshazzo@gmail.com>
2025-08-10 14:35:35 +0500

    add api

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "./components/ui/button";
 import Navbar from "./components/Mycomponents/Navbar";
 import Hero from "./components/Mycomponents/Hero";
 import Card from "./components/Mycomponents/Card";
 
 const App = () => {
-  return <div className="h-screen w-full bg-white">
-    <Navbar />
-    <Hero/>
-    <div className="products w-full bg-black min-h-screen">
-      <div className="text">
-        <h1 className="text-5xl text-start p-10 text-white">Products</h1>
-      </div>
-      <div className="cards p-10 flex flex-wrap gap-15">
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
+
+  const [products, setproducts] = useState([])
+
+  const getdata = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    const data = await res.json()
+    setproducts(data)
+  };
+
+  useEffect(() => {
+getdata()
+console.log(products);
+
+  }, [])
+  
+
+  return (
+    <div className="h-screen w-full bg-white">
+      <Navbar />
+      <Hero />
+      <div className="products w-full bg-black min-h-screen">
+        <div className="text">
+          <h1 className="text-5xl text-start p-10 text-white">Products</h1>
+        </div>
+        <div className="cards p-10 flex flex-wrap gap-15">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
       </div>
     </div>
-
-  </div>;
+  );
 };
 
 export default App;
