17e1b7b56afc96dcb74df66dd1d2e8dcc6bb2c79
shahzaib khan <shahzaibshazzo@gmail.com>
2025-08-10 14:25:05 +0500

    hero add

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { Button } from "./components/ui/button";
-import Card from "./components/Mycomponents/Card";
 import Navbar from "./components/Mycomponents/Navbar";
 import Hero from "./components/Mycomponents/Hero";
+import Card from "./components/Mycomponents/Card";
 
 const App = () => {
   return <div className="h-screen w-full bg-white">
     <Navbar />
     <Hero/>
-    <div className="products w-full bg-zinc-500 min-h-screen">
-      
+    <div className="products w-full bg-black min-h-screen">
+      <div className="text">
+        <h1 className="text-5xl text-start p-10 text-white">Products</h1>
+      </div>
+      <div className="cards">
+        <Card/>
+      </div>
     </div>
 
   </div>;
