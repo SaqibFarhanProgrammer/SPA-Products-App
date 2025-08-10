80c0c4cba12ae114af622a064e040f70a90bba60
saqib <saqibkhatai@gmail.com>
2025-08-10 01:14:06 -0700

    daisy ui and shadcn and tailwind installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Button } from "./components/ui/button";
+import Card from "./components/Mycomponents/Card";
 
 const App = () => {
   return (
     <div>
       <Button className="m-20"> hello</Button>
+
+      <Card />
     </div>
   );
 };
