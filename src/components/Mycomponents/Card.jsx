f77699b0f5a6003b90e4e70f4067add7e07bdfe3
shahzaib khan <shahzaibshazzo@gmail.com>
2025-08-10 14:55:41 +0500

    add api

--- src/components/Mycomponents/Card.jsx
+++ src/components/Mycomponents/Card.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { Button } from "../ui/button";
+import { Slice } from "lucide-react";
 
-const Card = () => {
+const Card = ({title , dec , price , src, }) => {
+  console.log(title);
+
+
+  
   return (
     <div>
       <div className="card bg-zinc-100 w-96 shadow-sm">
@@ -13,12 +18,11 @@ const Card = () => {
         </figure>
         <div className="card-body">
           <h2 className="card-title">
-            Card Title
+            {title}
             <div className="badge badge-secondary">NEW</div>
           </h2>
           <p>
-            A card component has a figure, a body part, and inside body there
-            are title and actions parts
+         {dec.Slice(0, 20)}
           </p>
           <div className="card-actions justify-end mt-10">
             <Button>
